12126018100602589aef16c63ee30bc7ba529297
Ben D'Angelo <ben.dangelo2@gmail.com>
2013-04-08 18:49:12 -0400

    added gitignore

--- link.sh
+++ link.sh
@@ -4,4 +4,8 @@ SCRIPT=$(readlink -f 0)
 SCRIPTPATH=$(dirname $SCRIPT)
 
 rm ~/.gitconfig
+rm ~/.gitignore
+
 ln -s $SCRIPTPATH/git/gitconfig ~/.gitconfig
+
+ln -s $SCRIPTPATH/git/gitignore ~/.gitignore
