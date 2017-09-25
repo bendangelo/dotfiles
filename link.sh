485ffbb6db7f5ec5fea6889721d6950cec3a2a04
Ben D'Angelo <ben.dangelo2@gmail.com>
2017-09-25 13:43:01 -0400

    Commented out inputrc was only for linux, cleaned up paths in zsh

--- link.sh
+++ link.sh
@@ -7,7 +7,7 @@ ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
 ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/agignore ~/.agignore
 ln -sf ~/dotfiles/vimrc ~/.vimrc
-ln -sf ~/dotfiles/inputrc ~/.inputrc
+# ln -sf ~/dotfiles/inputrc ~/.inputrc
 ln -sf ~/dotfiles/bash/bash_aliases ~/.bash_aliases
 
 echo "Done"
